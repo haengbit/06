de424ced472cfbcfa586890731528d60c58858ed
haengbit <biit4894@sookmyung.ac.kr>
2022-10-11 17:15:14 +0900

    6주차_실습2

--- main.c
+++ main.c
@@ -3,21 +3,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void print_star()
+
+/*½Ç½À 2¹ø */
+
+void func(void)
 {
-	int i;
-	for (i = 0; i < 10; i++) 
-		printf("*");
-}
-int main(void) 
-{
-	print_star();
-	print_star();
-	print_star();
-	
-	return 0;
+	int x;
+	printf("func x is at %p\n", &x);
 }
 
+int main(int argc, char *argv[]) {
+	int x;
+	printf("main x is at %p\n", &x);
+	func();
+	return 0;
+}
 
 
 
